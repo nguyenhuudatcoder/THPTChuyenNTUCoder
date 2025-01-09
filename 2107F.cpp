005014176e7080ec439d46d7f9fd374bc232f81a
nguyenhuudatcoder <nguyenhuudatnoob2010@gmail.com>
2025-01-09 10:15:40 +0700

    Accepted

--- 2107F.cpp
+++ 2107F.cpp
@@ -1,11 +1,11 @@
-///Made by Nguyễn Hữu Đạt
-#include<bits/stdc++.h>
+/// Made by Nguyễn Hữu Đạt
+#include <bits/stdc++.h>
 #define ll long long
 #define kien main
 using namespace std;
-const ll inf=LLONG_MAX;
-const ll mod=1e9+7;
-int a;
+const ll inf = LLONG_MAX;
+const ll mod = 1e9 + 7;
+int n, a[100005], t, k;
 kien()
 {
     // if(fopen(".inp","r"))
@@ -16,5 +16,22 @@ kien()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-
+    cin >> n;
+    cin >> t;
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    sort(a + 1, a + 1 + n);
+    while (t--)
+    {
+        cin >> k;
+        int pos = lower_bound(a + 1, a + 1 + n, k) - a;
+        pos--;
+        int i = 0;
+        while (k - a[pos] + i < pos && pos > 0)
+        {
+            i += k - a[pos];
+            pos--;
+        }
+        cout << n - pos << "\n";
+    }
 }
