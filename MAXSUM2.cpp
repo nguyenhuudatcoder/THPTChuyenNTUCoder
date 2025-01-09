c76eb5a9151862a652073841865f71f3f4cca6f0
nguyenhuudatcoder <nguyenhuudatnoob2010@gmail.com>
2025-01-09 22:35:01 +0700

    Accepted

--- MAXSUM2.cpp
+++ MAXSUM2.cpp
@@ -5,7 +5,9 @@
 using namespace std;
 const ll inf=LLONG_MAX;
 const ll mod=1e9+7;
-
+int n;
+ll ans;
+ll x,s;
 kien()
 {
     // if(fopen(".inp","r"))
@@ -16,5 +18,11 @@ kien()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>x;
+        ans+=abs(x);
+        s+=x;
+    }cout<<ans+s;
 }
