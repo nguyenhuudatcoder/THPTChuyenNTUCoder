55a31605d630b2b757a18bb8cdba25363d7997d6
nguyenhuudatcoder <nguyenhuudatnoob2010@gmail.com>
2025-01-09 13:22:55 +0700

    Declined

--- TPOWER.cpp
+++ TPOWER.cpp
@@ -1,11 +1,13 @@
-///Made by Nguyễn Hữu Đạt
-#include<bits/stdc++.h>
+/// Made by Nguyễn Hữu Đạt
+#include <bits/stdc++.h>
 #define ll long long
 #define kien main
 using namespace std;
-const ll inf=LLONG_MAX;
-const ll mod=1e9+7;
-
+const ll inf = LLONG_MAX;
+const ll mod = 1e9 + 7;
+unordered_map<unsigned int, ll> dem;
+ll ans[40];
+int n, a[100005];
 kien()
 {
     // if(fopen(".inp","r"))
@@ -16,5 +18,22 @@ kien()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-
+    int k;
+    cin >> n >> k;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        int s = 1;
+        for (int p = 1; p <= min(32, k); p++)
+        {s*=2;
+            if(s-a[i]>=0)
+            ans[p] += 1ll * dem[s - a[i]];
+        }
+        dem[a[i]]++;
+    }
+    for (int i = 1; i <= k; i++)
+        cout << ans[i] << "\n";
 }
