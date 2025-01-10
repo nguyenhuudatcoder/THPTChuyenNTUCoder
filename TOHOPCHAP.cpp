f163a67aea530e0478c6e60b7b02ad7d00a7af45
nguyenhuudatcoder <nguyenhuudatnoob2010@gmail.com>
2025-01-11 00:04:00 +0700

    Accepted

--- TOHOPCHAP.cpp
+++ TOHOPCHAP.cpp
@@ -1,15 +1,32 @@
-///Made by Nguyễn Hữu Đạt
-#include<bits/stdc++.h>
+/// Made by Nguyễn Hữu Đạt
+#include <bits/stdc++.h>
 #define ll long long
 #define kien main
 using namespace std;
-const ll inf=LLONG_MAX;
-const ll mod=1e9+7;
-int n,k;
-ll a[10];
-void ChatGPT()
+const ll inf = LLONG_MAX;
+const ll mod = 1e9 + 7;
+int n, k, pos;
+ll a[10], ans[10], res;
+void in()
 {
-    
+    for (int i = 0; i < k; i++)
+        cout << ans[i] << " ";
+    cout << "\n";
+}
+void ChatGPT(int m)
+{
+    for (int i = pos; i <= n; i++)
+    {
+        pos = i + 1;
+        if (m == k)
+        {
+            res++;
+            in();
+            return;
+        }
+        ans[m] = a[i];
+        ChatGPT(m + 1);
+    }
 }
 kien()
 {
@@ -21,5 +38,9 @@ kien()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-
+    cin >> k >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];sort(a,a+n);
+    ChatGPT(0);
+    cout << res;
 }
