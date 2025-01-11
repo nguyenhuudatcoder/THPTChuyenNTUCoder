432927841921970b314e0ed04edb6b659385273e
nguyenhuudatcoder <nguyenhuudatnoob2010@gmail.com>
2025-01-11 15:11:47 +0700

    Declined

--- CHHOPCHAP.cpp
+++ CHHOPCHAP.cpp
@@ -1,11 +1,35 @@
-///Made by Nguyễn Hữu Đạt
-#include<bits/stdc++.h>
+/// Made by Nguyễn Hữu Đạt
+#include <bits/stdc++.h>
 #define ll long long
 #define kien main
 using namespace std;
-const ll inf=LLONG_MAX;
-const ll mod=1e9+7;
-int n,k;
+const ll inf = LLONG_MAX;
+const ll mod = 1e9 + 7;
+int n, k;
+ll a[25], ans;
+bool use[25];
+ll b[25];
+void in()
+{
+    ans++;
+    for (int i = 0; i < k; i++)
+        cout << b[i] << " ";
+    cout << "\n";
+}
+void ChatGPT(int j)
+{
+    if(j==k){in();return;}
+    for (int i = 0; i < n; i++)
+    {
+        if (use[i] == 0)
+        {
+            b[j] = a[i];
+            use[i] = 1;
+            ChatGPT(j+1);
+            use[i]=0;
+        }
+    }
+}
 kien()
 {
     // if(fopen(".inp","r"))
@@ -16,5 +40,6 @@ kien()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-
+    cin>>k>>n;for(int i=0;i<n;i++)cin>>a[i];
+    ChatGPT(0);cout<<ans;
 }
