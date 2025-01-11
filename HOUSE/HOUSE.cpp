12035f7166ec6229c4ded92e8e64050a577728be
nguyenhuudatcoder <nguyenhuudatnoob2010@gmail.com>
2025-01-11 22:18:13 +0700

    Declined

---
+++ HOUSE/HOUSE.cpp
@@ -0,0 +1,31 @@
+/// Made by Nguyễn Hữu Đạt
+#include <bits/stdc++.h>
+#define ll long long
+#define kien main
+using namespace std;
+const ll inf = LLONG_MAX;
+const ll mod = 1e9 + 7;
+ll n, c[1000005], ans;
+kien()
+{
+    // if(fopen(".inp","r"))
+    // {
+    // 	freopen(".inp","r",stdin);
+    // 	freopen(".out","w",stdout);
+    // }
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> c[i];
+    for (int i = 1; i < n; i++)
+    {
+        if (c[i - 1] > c[i])
+            ans += c[i - 1] - c[i];
+    }
+    if (ans == 0)
+        cout << -1;
+    else
+        cout << ans;
+}
