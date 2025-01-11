9bef5c834c89e7c8575a757e8c995167bac35950
nguyenhuudatcoder <nguyenhuudatnoob2010@gmail.com>
2025-01-11 16:48:16 +0700

    Accepted

--- TPOWER.cpp
+++ TPOWER.cpp
@@ -5,9 +5,9 @@
 using namespace std;
 const ll inf = LLONG_MAX;
 const ll mod = 1e9 + 7;
-unordered_map<unsigned int, ll> dem;
-ll ans[40];
-int n, a[100005];
+ll ans[100005];
+unsigned ll n, a[100005];
+unsigned ll s = 1;
 kien()
 {
     // if(fopen(".inp","r"))
@@ -24,15 +24,20 @@ kien()
     {
         cin >> a[i];
     }
-    for (int i = 0; i < n; i++)
+    sort(a, a + n);
+    for (int p = 1; p <= min(k, 65); p++)
     {
-        int s = 1;
-        for (int p = 1; p <= min(32, k); p++)
-        {s*=2;
-            if(s-a[i]>=0)
-            ans[p] += 1ll * dem[s - a[i]];
+        s *= 2;
+        for (int i = n - 1; i > 0; i--)
+        {
+            if (s > a[i])
+            {
+                unsigned ll m = s - a[i];
+                int x = lower_bound(a, a + i , m) - a;
+                int y = upper_bound(a, a + i , m) - a;
+                ans[p] += y - x;
+            }
         }
-        dem[a[i]]++;
     }
     for (int i = 1; i <= k; i++)
         cout << ans[i] << "\n";
