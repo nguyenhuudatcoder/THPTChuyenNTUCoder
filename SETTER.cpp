78c7d0c9a58172ada2a5700884c4a7d6cef69aaf
nguyenhuudatcoder <nguyenhuudatnoob2010@gmail.com>
2025-01-14 21:31:16 +0700

    Accepted

---
+++ SETTER.cpp
@@ -0,0 +1,24 @@
+///Made by Nguyễn Hữu Đạt
+#include<bits/stdc++.h>
+#define ll long long
+#define kien main
+using namespace std;
+const ll inf=LLONG_MAX;
+const ll mod=1e9+7;
+int n,a,b,c;
+kien()
+{
+    // if(fopen(".inp","r"))
+    // {
+    // 	freopen(".inp","r",stdin);
+    // 	freopen(".out","w",stdout);
+    // }
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin>>a>>b>>c>>n;
+    if(a==0||b==0||c==0)cout<<"NO";
+    else if(n<3)cout<<"NO";
+    else if(a+b+c<n)cout<<"NO";
+    else cout<<"YES";
+}
