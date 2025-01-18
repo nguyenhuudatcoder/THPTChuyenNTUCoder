a89d71d042e3839308c886514e4e634308283e62
nguyenhuudatcoder <nguyenhuudatnoob2010@gmail.com>
2025-01-18 18:56:43 +0700

    Accepted

--- POW.cpp
+++ POW.cpp
@@ -1,33 +1,42 @@
-///Suck Kiên's brain is very good to your brain
-///Kiên will become Vegapunk
-///Made by Nguyễn Hữu Đạt
-#include<bits/stdc++.h>
-#define int long long
-#define mod 1000000007
+/// Made by Nguyễn Hữu Đạt
+#include <bits/stdc++.h>
+#define ll long long
+#define kien main
 using namespace std;
-int a,n,ans=1,t;
-main()
+const ll inf = LLONG_MAX;
+const ll mod = 1e9 + 7;
+ll a, n;
+// tham khảo modul nghịch đảo
+ll luythua(ll a, ll b)
 {
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a % mod;
+        a = a * a % mod;
+        b /= 2;
+    }
+    return res;
+}
+kien()
+{
+    // if(fopen(".inp","r"))
+    // {
+    // 	freopen(".inp","r",stdin);
+    // 	freopen(".out","w",stdout);
+    // }
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    cin>>a>>n;
-    if(a==1){
-        cout<<1+n;
-        return 0;
-    }
-    n++;
-    t=a%mod;
-    // ans=check(a,n);
-    while(n>0)
+    cin >> a >> n;
+    if (a == 1)
+        cout << (n+1 % mod);
+    else
     {
-        if(n%2==1)
-        {
-            ans=((ans%mod)*(t%mod))%mod;
-        }
-        n/=2;
-        t=((t%mod)*(t%mod))%mod;
+        ll m = luythua(a, n + 1);
+        m = (m - 1 + mod) % mod;
+        a = luythua(a-1, mod-2);
+        cout << m * a % mod;
     }
-    int m=pow(a-1,-1);
-    cout<<(ans-1)*m;
 }
--- QUEENS.cpp
+++ QUEENS.cpp
@@ -6,31 +6,31 @@ using namespace std;
 const ll inf = LLONG_MAX;
 const ll mod = 1e9 + 7;
 int a[10][10], ans = -2e9, s;
-bool l[20], r[20],collumn[10];
-void ChatGPT(int queen,int col,int line)
+bool l[20], r[20], collumn[10];
+void ChatGPT(int i)
 {
-    if(queen==9){
-        ans=max(s,ans);
+    if (i == 9)
+    {
+        // cout << "\n";
+        ans = max(s, ans);
         return;
     }
-    if(collumn[col]==1)return;
-    if (l[col + line] == 1)
-        return;
-    if (r[8 - col + line] == 1)
-        return;
-    for (int i = line; i <= 8; i++)
-        for (int j = 1; j <= 8; j++)
+    for (int j = 1; j <= 8; j++)
+    {
+        if (collumn[j] == 0 && r[i-j] == 0 && l[i+j] == 0)
         {
             l[j + i] = 1;
-            r[8 - j + i] = 1;
-            collumn[j]=1;
-            s += a[j][i];
-            ChatGPT(queen+1,j,i+1);
-            s -= a[j][i];
-            collumn[j]=0;
+            r[i - j] = 1;
+            collumn[j] = 1;
+            s += a[i][j];
+            // cout << i << j << " ";
+            ChatGPT(i+1);
+            s -= a[i][j];
+            collumn[j] = 0;
             l[j + i] = 0;
-            r[8 - i + i] = 0;
+            r[i - j] = 0;
         }
+    }
 }
 kien()
 {
@@ -45,6 +45,6 @@ kien()
     for (int i = 1; i <= 8; i++)
         for (int j = 1; j <= 8; j++)
             cin >> a[i][j];
-    ChatGPT(1,1,1);
-    cout<<ans;
+    ChatGPT(1);
+    cout << ans;
 }
